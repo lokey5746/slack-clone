6dfedef03e38c951103d9d6bdfd6726210014b59
Lokendra Dangi <pachar.com@gmail.com>
2020-09-02 22:28:54 +0545

    Setup google auth provider

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,8 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
+const auth = firebase.auth();
 const storage = firebaseApp.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, auth, storage };
+export { db, auth, storage, provider };
