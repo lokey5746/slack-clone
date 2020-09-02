e3d3d70c2a0c797c4759e34759371f7447a8e9f1
Lokendra Dangi <pachar.com@gmail.com>
2020-09-02 22:26:01 +0545

    Sidebar Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h2>Let's Start Slack Clone</h2>
-      {/* Header component */}
       <Header />
-      {/* Sidebar component */}
-      {/* Chat Screen(React Router) */}
+      <div className="app__body">
+        <Sidebar />
+        {/* Sidebar component */}
+        {/* Chat Screen(React Router) */}
+      </div>
     </div>
   );
 }
