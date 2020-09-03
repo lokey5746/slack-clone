732574d964f9d567dfd284584223aba2595918f1
Lokendra Dangi <pachar.com@gmail.com>
2020-09-03 08:03:51 +0545

    Add channel is complete

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
+// React router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./Chat";
+
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* Sidebar component */}
-        {/* Chat Screen(React Router) */}
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h2>Welcome</h2>
+            </Route>
+          </Switch>
+          {/* Chat Screen(React Router) */}
+        </div>
+      </Router>
     </div>
   );
 }
